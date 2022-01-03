c1c792a10dd69d60528a7274cc3821b1d80edc25
Radek Svoboda <radek.svoboda@vsb.cz>
2022-01-03 17:10:22 +0100

    Tailwind CSS template ready

--- hyperfei-frontend/src/App.js
+++ hyperfei-frontend/src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { MenuBar } from "./Components/MenuBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div data-theme='emerald'>
+        <MenuBar />
+        <div class="hero min-h-screen bg-base-200">
+        <div class="text-center hero-content">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold text-primary">
+                  Hello there
+                </h1> 
+            <p class="mb-5">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                </p> 
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    <div className='container mx-auto'>
 
-      <br />
-      <button class="btn btn-primary">daisyUI Button</button>
+          <h1 className="text-3xl">
+            Hello world!
+          </h1>
 
-    </div>
+          <br />
+          <button className="btn btn-primary">daisyUI Button</button>
+          
+        </div>
+      </div>
   );
 }
 
