4ab330f67fa171299472608fe502cd1760b4998b
Radek Svoboda <rasvob14@gmail.com>
2022-01-12 16:43:31 +0100

    Printing results added

--- hyperfei-frontend/src/Components/ResultsScreen.js
+++ hyperfei-frontend/src/Components/ResultsScreen.js
@@ -1,5 +1,4 @@
 import { useReactToPrint } from 'react-to-print';
-import QRCode  from "qrcode.react";
 import { useState, useRef, forwardRef, useEffect } from "react";
 import { useRecoilState, useResetRecoilState } from "recoil";
 import { postSelectedImage } from "../DAL/APIGateway";
@@ -7,54 +6,8 @@ import { videoCaptureState, selectedImageState, styleTransferedImagesState } fro
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import { Instagram } from 'react-content-loader';
-
-export const ComponentToPrint = forwardRef((props, ref) => {
-    const [videoState] = useRecoilState(videoCaptureState);
-    const [styleTransferedImages, setStyleTransferedImages] = useRecoilState(styleTransferedImagesState);
-
-    return (
-      <div ref={ref} className="flex flex-col h-screen">
-        <div className="">
-            <img src='460 FEI-CZ.png' className="w-2/3"></img>
-        </div>
-        <div className="flex-grow ">
-            <div className="grid grid-cols-2 gap-6 justify-items-center container w-2/3 mx-auto">
-                {
-                    videoState.slice(0, 4).map((x, i) => (
-                        <div className="border-b-4 border-vsb">
-                            <img alt="" src={x} key={`img_gallery_${i}`} className="img" />
-                        </div>
-                    ))
-                }
-            </div>
-
-            <div className="bg-vsb my-5">
-                <div className="mx-auto w-2/3 py-3">
-                    <h1 className="text-white text-4xl font-bold">Pojď studovat k nám!</h1>
-                    <div className="flex-row flex">
-                        <div className="flex-1 mr-3">
-                            <p className="font-sans text-white text-sm text-opacity-80 my-3">Náš svět se skládá z jedniček a nul. Jednotvárný ale není, neboj. Každý den je jiný. Každá aplikace, každý web nebo operační systém si hodinu od hodiny žádají nové nápady.</p>
-                            <p className="font-sans text-white text-sm text-opacity-80 my-3">Mezi „softwary, hardwary, bity i byty“ jsme jako ryby ve vodě. A pokud je i tobě v tomhle světě dobře, u nás se určitě neztratíš. Naopak. Jestli toužíš po studiu, které má smysl, jsi na správné adrese.</p>
-                        </div>
-                        <div className="flex items-center">
-                            <QRCode value="https://www.studujvostrave.cz/studijni-obory/informatika/" size={156}></QRCode>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="mx-auto w-2/3 flex justify-center">
-                    <h1 className="font-sans bg-white color-vsb text-xl font-bold uppercase p-3 mb-3">Nascanuj QR kód a dozvi se více!</h1>
-                </div>
-            </div>
-        </div>
-        
-        <div className="ml-6 mb-4">
-            <img src='pdf_footer.png' className="w-2/3" alt="print-footer" />
-        </div>
-      </div>
-    );
-  });
+import ComponentToPrint from './ComponentToPrint';
+import ImageResultComponent from "./ImageResultComponent";
 
 const PrintButton = () => {
     const componentRef = useRef();
@@ -77,21 +30,7 @@ const PrintButton = () => {
     );
   };
 
-const ImageResultComponent = ({image, title}) => {
-    return (
-        <div>
-            <div className="indicator">
-            <div className="indicator-item badge badge-primary">{title}</div>
 
-            {
-                image 
-                ? <img className='img rounded-md' alt='Image after style transfer' src={image} /> 
-                : <Instagram width={372} height={372} />
-            }
-            </div>
-        </div>
-    );
-};
 
 export default function ResultsScreen() {
     const [selectedImage, setSelectedImage] = useRecoilState(selectedImageState);
