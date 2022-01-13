b6c18a036a32f5c682a3118bb87d6cae86f63a94
Radek Svoboda <rasvob14@gmail.com>
2022-01-13 12:29:44 +0100

    Dotenv added

--- hyperfei-frontend/src/DAL/APIGateway.js
+++ hyperfei-frontend/src/DAL/APIGateway.js
@@ -1,5 +1,6 @@
 
-const API_URL = "http://127.0.0.1:9000";
+// const API_URL = "http://127.0.0.1:9000";
+const API_URL = process.env.REACT_APP_API_URL;
 const TEST_URL = `${API_URL}/active`
 const POST_IMAGE_URL = `${API_URL}/transform`
 
