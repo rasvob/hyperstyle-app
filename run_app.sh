b82e5dc00bf75574b76d898e59f47125a5ae945e
Radek Svoboda <rasvob14@gmail.com>
2023-08-24 11:00:30 +0200

    Docker build and deploy added

--- run_app.sh
+++ run_app.sh
@@ -6,5 +6,6 @@ PORT=9000
 fuser -k $PORT/tcp
 
 ./venv/bin/uvicorn main:app --host 158.196.145.25 --port $PORT --access-log --ssl-keyfile ./hyperfei-frontend/certs/MyKey.key  --ssl-certfile ./hyperfei-frontend/certs/MyCertificate.crt
+# uvicorn main:app --host 0.0.0.0 --port 10000 --log-level debug
 # --ssl-keyfile ./hyperfei-frontend/certs/MyKey.key
-deactivate
+# deactivate
